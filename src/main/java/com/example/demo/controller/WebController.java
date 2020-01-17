112d2c08dbc74b1a7a01c506a4182a3a1b932515
Kunal Sachdeva <likekunal.8@gmail.com>
2020-01-18 00:37:25 +0530

    changes commiting

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -2,11 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WebController {
     @RequestMapping(value="/index")
     public String index(){
         return "index";
+    }
+    @RequestMapping(value = "/validate")
+    @ResponseBody
+    public String validate(){
+        return "You are logged in";
     }
 }
